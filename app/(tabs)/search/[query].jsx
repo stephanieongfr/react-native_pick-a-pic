894b08cab10af51e7615893a62969efe289f0b5f
Stephanie Ong <135809652+stephanieongfr@users.noreply.github.com>
2024-05-18 23:17:55 +0200

    fix: refresh search

--- app/(tabs)/search/[query].jsx
+++ app/(tabs)/search/[query].jsx
@@ -1,6 +1,6 @@
 import { useLocalSearchParams } from "expo-router";
 import { useState, useEffect } from "react";
-import { Alert, FlatList, Image, View, Text } from "react-native";
+import { ActivityIndicator, Alert, FlatList, Image, View, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import SearchInput from "../../../components/SearchInput.jsx";
@@ -8,13 +8,12 @@ import EmptyState from "../../../components/EmptyState.jsx";
 import logo from "../../../assets/images/logo.png";
 import PhotoCard from "../../../components/PhotoCard.jsx";
 
-const BASE_URL = "https://api.unsplash.com";
-const KEY = "RLBj31QckyPf6v8e6dlcCVlB6-NuWfBFiZ_zfGDgkVY";
+const BASE_URL = process.env.EXPO_PUBLIC_API_URL;
+const KEY = process.env.EXPO_PUBLIC_API_KEY;
 
 const Search = () => {
   const { query } = useLocalSearchParams();
   const [searchedPhotos, setSearchedPhotos] = useState([]);
-
   const [isLoading, setIsLoading] = useState(true);
 
   const getSearchedPhotos = async () => {
@@ -25,9 +24,8 @@ const Search = () => {
       if (!result.ok) throw new Error("Could not find photos");
       
       const data = await result.json();
-      console.log(data);
-      console.log(data.results);
       setSearchedPhotos(data.results);
+
     } catch (error) {
       Alert.alert("Error", error.message);
     } finally {
@@ -36,48 +34,54 @@ const Search = () => {
   }
   
   useEffect(() => {
-    getSearchedPhotos()
-  }, []);
+    getSearchedPhotos(query)
+  }, [query]);
 
 
   return (
     <SafeAreaView className="h-full">
-      <FlatList
-        data={searchedPhotos}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => (
-          <PhotoCard photo={item} />
-        )}
-        ListHeaderComponent={() => (
-          <View className="my-6 px-4 space-y-6">
-            <View className="justify-between items-start flex-row mb-6">
-              <View>
-                <Text className="font-pmedium text-sm">
-                  Search Results
-                </Text>
+      {isLoading ? (
+        <View className="flex-1 justify-center items-center">
+          <ActivityIndicator size="large" color="#AA1781" />
+        </View>
+      ) : (
+        <FlatList
+          data={searchedPhotos}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <PhotoCard photo={item} />
+          )}
+          ListHeaderComponent={() => (
+            <View className="my-6 px-4 space-y-6">
+              <View className="justify-between items-start flex-row mb-6">
+                <View>
+                  <Text className="font-pmedium text-sm">
+                    Search Results
+                  </Text>
+                </View>
+
+                <View className="mt-1.5">
+                  <Image
+                    source={logo}
+                    alt="logo of pick a pic"
+                    className="w-9 h-10"
+                    resizeMode="contain"
+                  />
+                </View>
               </View>
 
-              <View className="mt-1.5">
-                <Image
-                  source={logo}
-                  alt="logo of pick a pic"
-                  className="w-9 h-10"
-                  resizeMode="contain"
-                />
-              </View>
+              <SearchInput />
+
             </View>
-
-            <SearchInput />
-
-          </View>
-        )}
-        ListEmptyComponent={() => (
-          <EmptyState
-            title="No photos Found"
-            subtitle="No photos found for this search query"
-          />
-        )}
-      />
+          )}
+          ListEmptyComponent={() => (
+            <EmptyState
+              title="No photos Found"
+              subtitle="No photos found for this search query"
+            />
+          )}
+        />
+      )}
     </SafeAreaView>
   )
 };
